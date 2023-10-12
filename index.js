bd8bd7bd035914d00d388836ae8ede98b1b43741
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-10-12 20:20:20 +0400

    configurate DB into its own module - module/note.js file

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
 import {} from 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
-
-const url = process.env.MONGODB_URI;
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
+import Note from './models/note.js';
 
 const app = express();
 
