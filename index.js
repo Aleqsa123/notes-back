55e9b2ad71107b71bedc4c2e13cdd13b0f073181
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-10-13 11:24:08 +0400

    modify index.js, add error hanler to individual note's GET request

--- index.js
+++ index.js
@@ -19,11 +19,20 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-//GET - get individual note
+//GET - get individual note with error handlers
 app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
