5f4623e9502d89f562c61bdf562a5f9b633c98ec
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-10-13 11:59:00 +0400

    add DELETE request but not implemented in FRONT yet

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/api/notes', (request, response) => {
 })
 
 //GET - get individual note with error handlers
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
       if (note) {
@@ -29,20 +29,35 @@ app.get('/api/notes/:id', (request, response) => {
         response.status(404).end()
       }
     })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
+    .catch(error => next(error))
+})
+
+//DELETE request - removes individual note
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+//PUT request - changes importance of the note
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
-  response.status(204).end()
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
-//POST - add a new note to DB
+//POST request - add a new note to DB
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
@@ -59,6 +74,25 @@ app.post('/api/notes', (request, response) => {
     response.json(savedNote)
   })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+
+//handler of requests with result to errors -
+//this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
   app.listen(PORT, () => {
