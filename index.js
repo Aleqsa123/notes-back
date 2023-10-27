09515decd4a92c9ccbfa966248e7dd0c79cf0230
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-10-27 21:37:43 +0400

    establish project structure

--- index.js
+++ index.js
@@ -1,103 +1,33 @@
-import {} from "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+import config from "./utils/config.js";
+import logger from "./utils/logger.js";
+import middleware from "./utils/middleware.js";
 import cors from "cors";
-import Note from "./models/note.js";
+import notesRouter from "./controllers/notes.js";
 
 const app = express();
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info("connected to MongoDB");
+	})
+	.catch((error) => {
+		logger.error("error connecting to MongoDB:", error.message);
+	});
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static("build"));
+app.use(middleware.requestLogger);
 
-app.get("/", (request, response) => {
-	response.send("<h1>Hello World!</h1>");
-});
+app.use("/api/notes", notesRouter);
 
-app.get("/api/notes", (request, response) => {
-	Note.find({}).then(notes => {
-		response.json(notes);
-	});
-});
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-//GET - get individual note with error handlers
-app.get("/api/notes/:id", (request, response, next) => {
-	Note.findById(request.params.id)
-		.then(note => {
-			if (note) {
-				response.json(note);
-			} else {
-				response.status(404).end();
-			}
-		})
-		.catch(error => next(error));
-});
-
-//DELETE request - removes individual note
-app.delete("/api/notes/:id", (request, response, next) => {
-	Note.findByIdAndRemove(request.params.id)
-		.then(() => {
-			response.status(204).end();
-		})
-		.catch(error => next(error));
-});
-
-//PUT request - changes importance of the note
-app.put("/api/notes/:id", (request, response, next) => {
-	const { content, important } = request.body;
-
-	Note.findByIdAndUpdate(
-		request.params.id,
-		{ content, important },
-		{ new: true, runValidators: true, context: "query" }
-	)
-		.then(updatedNote => {
-			response.json(updatedNote);
-		})
-		.catch(error => next(error));
-});
-
-//POST request - add a new note to DB
-app.post("/api/notes", (request, response, next) => {
-	const body = request.body;
-
-	if (body.content === undefined) {
-		return response.status(400).json({ error: "content missing" });
-	}
-
-	const note = new Note({
-		content: body.content,
-		important: body.important || false,
-	});
-
-	note.save()
-		.then(savedNote => {
-			response.json(savedNote);
-		})
-		.catch(error => next(error));
-});
-
-const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: "unknown endpoint" });
-};
-
-// handler of requests with unknown endpoint
-app.use(unknownEndpoint);
-
-const errorHandler = (error, request, response, next) => {
-	console.error(error.message);
-	if (error.name === "CastError") {
-		return response.status(400).send({ error: "malformatted id" });
-	} else if (error.name === "ValidationError") {
-		return response.status(400).json({ error: error.message });
-	}
-	next(error);
-};
-
-//handler of requests with result to errors -
-//this has to be the last loaded middleware.
-app.use(errorHandler);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+	logger.info(`Server running on port ${config.PORT}`);
 });
--- models/note.js
+++ models/note.js
@@ -2,18 +2,6 @@
 
 import mongoose from "mongoose";
 
-mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URI;
-
-mongoose.connect(url)
-	.then(() => {
-		console.log("connected to MongoDB");
-	})
-	.catch((error) => {
-		console.log("error connecting to MongoDB:", error.message);
-	});
-
 const noteSchema = new mongoose.Schema({
 	content: {
 		type: String,
